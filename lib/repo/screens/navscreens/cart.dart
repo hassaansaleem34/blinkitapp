398ee6b75779c99fd796f8cbae07d6c08c94ea49
Hassaan Saleem <hassaan@example.com>
2025-12-31 23:09:10 +0500

    Updated UI / bug fix / new changes

--- lib/repo/screens/navscreens/cart.dart
+++ lib/repo/screens/navscreens/cart.dart
@@ -1,11 +1,210 @@
+import 'package:blinkitapp/repo/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class cartscreen extends StatelessWidget {
-  const cartscreen({super.key});
+  TextEditingController searchcontrol = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("cart screen")));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              child: Stack(
+                children: [
+                  Container(
+                    height: 160,
+                    width: double.infinity,
+
+                    decoration: BoxDecoration(
+                      color: Color(0xffF7CB45),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+
+                    child: Column(
+                      children: [
+                        SizedBox(height: 30),
+                        Row(
+                          children: [
+                            SizedBox(height: 0, width: 10),
+                            uihelper.Customtext(
+                              text: "Blinkit in",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontsize: 12,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(height: 10, width: 7),
+                            uihelper.Customtext(
+                              text: "16 minutes",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.w700,
+                              fontsize: 20,
+                              fontfamily: "bold",
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+
+                        Row(
+                          children: [
+                            SizedBox(height: 10, width: 10),
+                            uihelper.Customtext(
+                              text: "HOME-",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontsize: 12,
+                            ),
+                            uihelper.Customtext(
+                              text: "Sujal Dave, Ratanada, Jodhpur (Raj)",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.normal,
+                              fontfamily: "regular",
+                              fontsize: 12,
+                            ),
+                            SizedBox(width: 10),
+                            uihelper.CustomImage(
+                              img: "arrow-down-sign-to-navigate 1.png",
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 30,
+                    bottom: 70,
+
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, color: Colors.black, size: 15),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    left: 40,
+                    child: uihelper.customsearchbar(controller: searchcontrol),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            uihelper.CustomImage(img: "shopping-cart (1) 1.png"),
+            SizedBox(height: 20),
+            uihelper.Customtext(
+              text: "Reordering will be easy",
+              color: Color(0xff000000),
+              fontweight: FontWeight.bold,
+              fontsize: 16,
+            ),
+            uihelper.Customtext(
+              text: "Items you order will show up here so you can buy",
+              color: Color(0xff000000),
+              fontweight: FontWeight.normal,
+              fontsize: 10,
+            ),
+            uihelper.Customtext(
+              text: "them again easily.",
+              color: Color(0xff000000),
+              fontweight: FontWeight.normal,
+              fontsize: 10,
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                uihelper.Customtext(
+                  text: "Bestsellers",
+                  color: Color(0xff000000),
+                  fontweight: FontWeight.bold,
+                  fontsize: 16,
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        uihelper.CustomImage(img: "image 45.png"),
+                        Padding(
+                          padding: EdgeInsets.only(top: 95, left: 70),
+                          child: uihelper.customadd(),
+                        ),
+                      ],
+                    ),
+                    uihelper.Customtext(
+                      text: "Amul Taaza Toned",
+                      color: Color(0xff000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 8,
+                    ),
+                    uihelper.Customtext(
+                      text: "Fresh Milk",
+                      color: Color(0xff000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 8,
+                    ),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        uihelper.CustomImage(img: "image 44.png"),
+                        Padding(
+                          padding: EdgeInsets.only(top: 95, left: 70),
+                          child: uihelper.customadd(),
+                        ),
+                      ],
+                    ),
+                    uihelper.Customtext(
+                      text: "POTATO",
+                      color: Color(0xff000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 8,
+                    ),
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        uihelper.CustomImage(img: "image 46.png"),
+                        Padding(
+                          padding: EdgeInsets.only(top: 95, left: 70),
+                          child: uihelper.customadd(),
+                        ),
+                      ],
+                    ),
+                    uihelper.Customtext(
+                      text: "Tomatoezzz",
+                      color: Color(0xff000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 8,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/repo/widgets/uihelper.dart
+++ lib/repo/widgets/uihelper.dart
@@ -22,4 +22,50 @@ class uihelper {
       ),
     );
   }
+
+  static customsearchbar({required TextEditingController controller}) {
+    return Container(
+      height: 37,
+      width: 346,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        border: Border.all(color: Color(0xffC5C5C5)),
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: "Search'ice-cream",
+          isDense: true, // 🔥 important
+          contentPadding: EdgeInsets.symmetric(vertical: 12),
+          prefixIcon: uihelper.CustomImage(
+            img: "search-interface-symbol 1.png",
+          ),
+          suffixIcon: uihelper.CustomImage(img: "mic 1.png"),
+        ),
+      ),
+    );
+  }
+
+  static customadd() {
+    return Container(
+      height: 18,
+      width: 30,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(1),
+        border: Border.all(color: Color(0xff27AF34)),
+      ),
+      child:Center(
+      
+      child: uihelper.Customtext(
+        text: "ADD",
+        color: Color(0xff27AF34),
+        fontweight: FontWeight.w400,
+        fontsize: 6,
+      ),
+      )
+    );
+    
+  }
 }
